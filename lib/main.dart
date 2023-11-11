3086ba1869d4fb2f7040f6d2601ddf18f8085f3e
Muhammad Waffi Qayyum <waffi121@gmail.com>
2023-11-11 12:53:00 +0800

    Revert "user can perform password recovery"
    This reverts commit ae2d94f761b5d57e1b1671fbe53b425c6c0be57f.

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,15 @@ import 'customer_pages/customer_changeProfilePicture.dart';
 import 'customer_pages/customer_deleteAccount.dart';
 import 'customer_pages/customer_hompage.dart';
 import 'customer_pages/customer_register.dart';
-import 'otpVerification.dart';
-import 'forgotPassword.dart';
-import 'resetPassword.dart';
 import 'login_page.dart';
 import 'rider_pages/rider_homepage.dart';
 import 'splash.dart';
-import 'package:uni_links/uni_links.dart';
-
 
 Future<void> main() async {
   await Supabase.initialize(
       url: 'https://bzscuwrolzmocdshaemx.supabase.co',
       anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6c2N1d3JvbHptb2Nkc2hhZW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzNTE3MDksImV4cCI6MjAxNDkyNzcwOX0.iGlxlb_WNLjh2apj3u9DDkvfl7d8hChLgd2qrIj6JJk',);
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6c2N1d3JvbHptb2Nkc2hhZW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkzNTE3MDksImV4cCI6MjAxNDkyNzcwOX0.iGlxlb_WNLjh2apj3u9DDkvfl7d8hChLgd2qrIj6JJk');
   runApp(MyApp());
 }
 void  clearUserSession(){
@@ -40,21 +35,17 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/', //used for changing between pages
       routes: <String, WidgetBuilder>{
-        '/': (_) => const SplashPage(),
+        // '/': (_) => const SplashPage(),
         '/register': (_) => const CustomerRegisterPage(),
-        '/login': (_) => const LoginPage(), //login_page
-
+        // '/login': (_) => const LoginPage(), //login_page
+        '/': (_) => const CustomerChangePicture(), //login_page
+        // '/custHome': (_) => const CustomerHomepage(),
+        // '/account': (_) => const AccountPage(),
 
 ////////////////////users homepage////////////////
         '/admin_home': (_) => const AdminHomePage(),
         '/rider_home': (_) => const RiderHomePage(),
         '/customer_home': (_) => const CustomerHomepage(),
-
-////////////////////reset password homepage////////////////
-        '/forgotPassword': (_) => const ForgotPassword(),
-        '/otpVerification': (_) => const OTPVerification(),
-        '/resetPassword': (_) => const ResetPassword()
-
 
       },
     );
