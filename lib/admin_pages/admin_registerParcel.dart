f505748116b432c1125cdab8d237fec84c025852
unknown <ryanrls4@icloud.com>
2023-12-19 00:40:52 +0800

    no suggestion for empty field

--- lib/admin_pages/admin_registerParcel.dart
+++ lib/admin_pages/admin_registerParcel.dart
@@ -350,101 +350,12 @@ class _AdminRegisterParcelState extends State<AdminRegisterParcel> {
                                 SizedBox(
                                   width: 20.0,
                                 ),
-                                // Container(
-                                //   width: 230,
-                                //   decoration: ShapeDecoration(
-                                //     color: Colors.white,
-                                //     shape: RoundedRectangleBorder(
-                                //       side: BorderSide(
-                                //           width: 4, color: Color(0xFF333333)),
-                                //       borderRadius: BorderRadius.circular(15),
-                                //     ),
-                                //   ),
-                                // child:
-                                // TypeAheadField(
-                                //   suggestionsCallback: (value) {
-                                //     return list_name
-                                //         .where((element) => element
-                                //             .toLowerCase()
-                                //             .contains(value.toLowerCase()))
-                                //         .toList();
-                                //   },
-                                //   builder: (context, controller, focusNode) {
-                                //     return Container(
-                                //       width: 230,
-                                //       decoration: ShapeDecoration(
-                                //         color: Colors.white,
-                                //         shape: RoundedRectangleBorder(
-                                //           side: BorderSide(
-                                //               width: 4,
-                                //               color: Color(0xFF333333)),
-                                //           borderRadius:
-                                //               BorderRadius.circular(15),
-                                //         ),
-                                //       ),
-                                //       child: TextFormField(
-                                //         controller: _customerName,
-                                //         textCapitalization:
-                                //             TextCapitalization.characters,
-                                //         focusNode: focusNode,
-                                //         validator: (value) {
-                                //           if (value == null || value.isEmpty) {
-                                //             return 'Please fill in';
-                                //           } else {
-                                //             return null;
-                                //           }
-                                //         },
-                                //         style: TextStyle(
-                                //           color: Color.fromARGB(255, 0, 0, 0),
-                                //           fontSize: 15,
-                                //           fontFamily: 'Lexend',
-                                //           fontWeight: FontWeight.w400,
-                                //         ),
-                                //         decoration: InputDecoration(
-                                //             hintText: 'enter name',
-                                //             filled: true,
-                                //             fillColor: const Color.fromARGB(
-                                //                 255, 249, 249, 249),
-                                //             border: OutlineInputBorder(
-                                //               // Use OutlineInputBorder for rounded borders
-                                //               borderRadius:
-                                //                   BorderRadius.circular(10),
-                                //               borderSide: BorderSide(
-                                //                 width: 0,
-                                //                 style: BorderStyle.none,
-                                //               ),
-                                //             ),
-                                //             contentPadding:
-                                //                 EdgeInsets.symmetric(
-                                //                     vertical: 12,
-                                //                     horizontal: 10)),
-                                //         // autofocus: true,
-                                //       ),
-                                //     );
-                                //   },
-                                //   hideOnEmpty: true,
-                                //   constraints: BoxConstraints(maxHeight: 500),
-                                //   itemBuilder: (context, String suggestion) {
-                                //     return Flexible(child: Text(suggestion));
-                                //   },
-                                //   onSelected: (String suggestion) {
-                                //     String selectPhone = "";
-                                //     for (var entry in list_user) {
-                                //       if (entry['name'] == suggestion) {
-                                //         entry['phone'] = selectPhone;
-                                //       }
-                                //     }
-                                //     setState(() {
-                                //       _customerName.text = suggestion;
-                                //       _phoneNumber.text = selectPhone;
-                                //     });
-                                //   },
-                                //   controller: _customerName,
-                                // ),
-
                                 TypeAheadField(
                                   suggestionsCallback: (value) {
                                     print('value : $value');
+                                    if (value.isEmpty) {
+                                      return List<String>.empty();
+                                    }
                                     return list_name
                                         .where((element) => element
                                             .toLowerCase()
